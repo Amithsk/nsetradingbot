bedec4f36c7c0beacdef4ee47e9a11d76c118ea5
Amithsk <amithsebkanat@gmail.com>
2025-09-18 11:18:02 +0530

    Updated the debug folder path

--- Code/nseintradaytradingdata.py
+++ Code/nseintradaytradingdata.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 OUTPUT_DIR = PROJECT_ROOT / "Output" / "Intraday"
-DEBUG_DIR = PROJECT_ROOT / "Output" / "Debug"
+DEBUG_DIR = PROJECT_ROOT /"debug"
 
 HOME_URL = "https://www.nseindia.com/"
 REPORTS_URL = "https://www.nseindia.com/all-reports"
